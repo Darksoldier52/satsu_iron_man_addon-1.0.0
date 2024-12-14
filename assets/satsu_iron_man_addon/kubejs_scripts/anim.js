43beef07ea8abc92aaf21e01e003b0dad2dfdd35
Satsu1312 <minion1312@hotmail.com>
2024-12-14 08:51:06 -0600

    test

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -5,7 +5,7 @@ PalladiumEvents.registerAnimations((event) => {
     const progress = animationUtil.getAnimationTimerAbilityValue(
       builder.getPlayer(),
       "satsu_iron_man_addon:kube_animations",
-      "omnitrix_main_arms_aim_one",
+      "test",
       builder.getPartialTicks()
     );
 
@@ -36,6 +36,56 @@ PalladiumEvents.registerAnimations((event) => {
           .setXRotDegrees(-112.7518)
           .setYRotDegrees(35.5821)
           .setZRotDegrees(-76.6749)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+        builder
+          .get("right_arm")
+          .setXRotDegrees(-85)
+          .setYRotDegrees(-37.5)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+      }
+    }
+  });
+});
+PalladiumEvents.registerAnimations((event) => {
+  event.register("satsu_iron_man_addon/personalization_mode_armor", 10, (builder) => {
+    // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+    const progress = animationUtil.getAnimationTimerAbilityValue(
+      builder.getPlayer(),
+      "satsu_iron_man_addon:kube_animations",
+      "omnitrix_main_arms_aim_one",
+      builder.getPartialTicks()
+    );
+
+    // only apply animation if progress is above 0!
+    if (progress > 0.0) {
+      const halfPi = 1.57079632679;
+
+      // if first person
+      if (builder.isFirstPerson()) {
+        builder
+          .get("left_arm")
+          .setXRotDegrees(-20)
+          .setYRotDegrees(-60)
+          .setZRotDegrees(60)
+          .animate("InOutCubic", progress);
+      }
+      if (builder.isFirstPerson()) {
+        builder
+          .get("right_arm")
+          .setXRotDegrees(-60)
+          .setYRotDegrees(-30)
+          .setZRotDegrees(-30)
+          .animate("InOutCubic", progress);
+      } else {
+        // third person animations
+        builder
+          .get("left_arm")
+          .setXRotDegrees(-55.3472)
+          .setYRotDegrees(24.2545)
+          .setZRotDegrees(18.3835)
           .animate("InOutCubic", progress);
         builder.get("chest").animate("InOutCubic", progress);
         builder
