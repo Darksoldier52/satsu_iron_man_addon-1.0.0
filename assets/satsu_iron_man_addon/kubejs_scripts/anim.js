75fa8c4b67da1ac7d20258731ff5d704c27b3d5c
Satsu1312 <minion1312@hotmail.com>
2025-01-29 16:33:31 -0600

    Fixing animation timer for mark 5

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -1125,9 +1125,7 @@ PalladiumEvents.registerAnimations((event) => {
         builder.getPlayer(),
         "satsu_iron_man_addon:kube_animations",
         "mark.5_anim",
-        builder.getPartialTicks(),
-        0,
-        3
+        builder.getPartialTicks()
       );
 
       // only apply animation if progress is above 0!
@@ -1208,93 +1206,6 @@ PalladiumEvents.registerAnimations((event) => {
     }
   );
   event.register(
-    "satsu_iron_man_addon/colocacion_de_armadura.8.mark.5.part.2",
-    10,
-    (builder) => {
-      // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
-      const progress = animationUtil.getAnimationTimerAbilityValue(
-        builder.getPlayer(),
-        "satsu_iron_man_addon:kube_animations",
-        "mark.5_anim",
-        builder.getPartialTicks(),
-        3,
-        6
-      );
-
-      // only apply animation if progress is above 0!
-      if (progress > 0.0) {
-        const halfPi = 1.57079632679; // if first person
-        if (builder.isFirstPerson()) {
-          builder
-            .get("left_arm")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
-            .setZRotDegrees(0)
-            .animate("InOutCubic", progress);
-        }
-        if (builder.isFirstPerson()) {
-          builder
-            .get("right_arm")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
-            .setZRotDegrees(0)
-            .animate("InOutCubic", progress);
-        } else {
-          // third person animations
-          builder
-            .get("left_arm")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
-            .setZRotDegrees(0)
-            .setX(0)
-            .setY(0)
-            .setZ(0)
-            .animate("InOutCubic", progress);
-          builder
-            .get("right_arm")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
-            .setZRotDegrees(0)
-            .setX(0)
-            .setY(0)
-            .setZ(0)
-            .animate("InOutCubic", progress);
-          builder
-            .get("head")
-            .setXRotDegrees(0)
-            .setY(0)
-            .setZ(0)
-            .animate("InOutCubic", progress);
-          builder
-            .get("chest")
-            .setXRotDegrees(0)
-            .setY(0)
-            .setZ(0)
-            .animate("InOutCubic", progress);
-            builder
-              .get("right_leg")
-              .setXRotDegrees(0)
-              .setYRotDegrees(0)
-              .setZRotDegrees(0)
-              .moveX(0)
-              .moveY(0)
-              .setZ(0)
-              .animate("InOutCubic", progress);
-            builder
-              .get("left_leg")
-              .setXRotDegrees(0)
-              .setYRotDegrees(0)
-              .setZRotDegrees(0)
-              .moveX(0)
-              .moveY(0)
-              .setZ(0)
-              .animate("InOutCubic", progress);
-          builder;
-        }
-      }
-    }
-  );
-  event.register(
     "satsu_iron_man_addon/colocacion_de_armadura.8.mark.5.part.3",
     10,
     (builder) => {
@@ -1302,10 +1213,8 @@ PalladiumEvents.registerAnimations((event) => {
       const progress = animationUtil.getAnimationTimerAbilityValue(
         builder.getPlayer(),
         "satsu_iron_man_addon:kube_animations",
-        "mark.5_anim",
-        builder.getPartialTicks(),
-        3,
-        6
+        "mark.5_anim.3",
+        builder.getPartialTicks()
       );
 
       // only apply animation if progress is above 0!
@@ -1332,57 +1241,14 @@ PalladiumEvents.registerAnimations((event) => {
           // third person animations
           builder
             .get("left_arm")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
             .setZRotDegrees(-90)
-            .moveX(0)
-            .moveY(0)
-            .moveZ(0)
             .animate("InOutCubic", progress);
           builder.get("chest").animate("InOutCubic", progress);
           builder
             .get("right_arm")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
             .setZRotDegrees(90)
-            .moveX(0)
-            .moveY(0)
-            .moveZ(0)
             .animate("InOutCubic", progress);
           builder.get("chest").animate("InOutCubic", progress);
-          builder
-            .get("head")
-            .setXRotDegrees(0)
-            .moveY(0)
-            .moveZ(0)
-            .animate("InOutCubic", progress);
-          builder
-            .get("chest")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
-            .setZRotDegrees(0)
-            .moveX(0)
-            .moveY(0)
-            .moveZ(0)
-            .animate("InOutCubic", progress);
-          builder
-            .get("right_leg")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
-            .setZRotDegrees(0)
-            .moveX(0)
-            .moveY(0)
-            .moveZ(0)
-            .animate("InOutCubic", progress);
-          builder
-            .get("left_leg")
-            .setXRotDegrees(0)
-            .setYRotDegrees(0)
-            .setZRotDegrees(0)
-            .moveX(0)
-            .moveY(0)
-            .moveZ(0)
-            .animate("InOutCubic", progress);
           builder;
         }
       }
--- data/satsu_iron_man_addon/functions/dummy.mcfunction
+++ data/satsu_iron_man_addon/functions/dummy.mcfunction
@@ -22,6 +22,7 @@ scoreboard objectives add satsu.iron.man.armor.ark.beam.color.icon dummy
 scoreboard objectives add satsu.iron.man.armor.energy.pulse.flame.thrower.icon dummy
 scoreboard objectives add satsu.iron.man.addon.altura dummy
 scoreboard objectives add satsu.iron.man.mask dummy
+scoreboard objectives add satsu.iron.man.animation.armor dummy
 scoreboard objectives add satsu.iron.man.armor.actived.perm dummy
 scoreboard objectives add satsu.iron.man.armor dummy
 scoreboard objectives add satsu.iron.man.armor.actived dummy
