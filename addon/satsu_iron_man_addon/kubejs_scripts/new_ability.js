bdbb7e0539ce55e6a60a37077732b67e45202494
Satsu1312 <minion1312@hotmail.com>
2025-07-24 20:43:34 -0600

    Start

--- addon/satsu_iron_man_addon/kubejs_scripts/new_ability.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/new_ability.js
@@ -107,3 +107,50 @@ StartupEvents.registry("palladium:abilities", (event) => {
       }
         });
 });
+StartupEvents.registry("palladium:abilities", (event) => {
+  // ID of the ability will be: 'kubejs:satsu_iron_man_addon/add_power'
+  event
+    .create("satsu_iron_man_addon:superpower")
+
+    // Preset icon, can also be changed individually in the power json
+    .icon(palladium.createItemIcon("minecraft:bread"))
+
+    // Documentation description
+    .documentationDescription(
+      "Add,set or remove a specific power to the player when the ability is active."
+    )
+    .addProperty(
+      "power_path",
+      "string",
+      "palladium",
+      "Path of the power to add"
+    )
+    .addProperty("powerid", "string", "test_power", "ID of the power to add")
+    .addProperty("capacity", "string", "add", "set,add or remove")
+    // Handler for what happens during EVERY tick of the ability being active, make sure to check the 'enabled' parameter
+    .tick((entity, entry, holder, enabled) => {
+      if (enabled) {
+        const power_path = entry.getPropertyByName("power_path");
+        const powerid = entry.getPropertyByName("powerid");
+        let capacity = entry.getPropertyByName("capacity");
+        if (capacity === "add") {
+          palladium.superpowers.addSuperpower(
+            entity,
+            `${power_path}:${powerid}`
+          );
+        }
+        if (capacity === "remove") {
+          palladium.superpowers.removeSuperpower(
+            entity,
+            `${power_path}:${powerid}`
+          );
+        }
+        if (capacity === "set") {
+          palladium.superpowers.setSuperpower(
+            entity,
+            `${power_path}:${powerid}`
+          );
+        }
+      }
+    });
+});
