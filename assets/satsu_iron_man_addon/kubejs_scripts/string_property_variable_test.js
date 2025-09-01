304c4a115e077faba2efc36e5277a9589a829ed0
Satsu1312 <minion1312@hotmail.com>
2025-09-01 15:39:37 -0600

    New colored beam

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -97,3 +97,17 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
       event.registerProperty('satsu_iron_man_addon_trail', 'string', "true");
     }
 });
+PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+      // Arguments: Key of the property, type of the property, default/starting value
+      event.registerProperty('satsu_iron_man_addon_beam_core_color', 'string', "d9f1ff");
+    }
+});
+PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+      // Arguments: Key of the property, type of the property, default/starting value
+      event.registerProperty('satsu_iron_man_addon_beam_glow_color', 'string', "3ab2d7");
+    }
+});
