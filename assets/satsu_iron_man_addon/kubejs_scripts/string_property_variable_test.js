8cb04e72e0033b7e4ebc76b9bffa4900ca3ecd6d
Satsu1312 <minion1312@hotmail.com>
2025-09-02 23:25:53 -0600

    Watch first things

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -97,3 +97,10 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
       event.registerProperty('satsu_iron_man_addon_trail', 'string', "true");
     }
 });
+PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+      // Arguments: Key of the property, type of the property, default/starting value
+      event.registerProperty('satsu_iron_man_addon_watch', 'string', "off");
+    }
+});
--- data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -98,3 +98,11 @@ PalladiumEvents.registerProperties((event) => {
     event.registerProperty("satsu_iron_man_addon_trail", "string", "true");
   }
 });
+
+PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_addon_watch", "string", "off");
+  }
+});
