2e4356b3ec6b724602bfa4c1dc6faff6f7c77644
Satsu1312 <minion1312@hotmail.com>
2025-09-13 19:28:32 -0600

    Update sounds.js

--- addon/satsu_iron_man_addon/kubejs_scripts/sounds.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/sounds.js
@@ -1,45 +1,123 @@
 StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:energyblast.throw");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:closed.helmet.throw");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:striker.dig");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:minigun");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:new_mask_sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:new_blast");
+});
+StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:rivals_iron_man_blast_mcu");
 });
 StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:rivals_iron_man_steampower_blast");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:new_blast");
+  event.create("satsu_iron_man_addon:new_beam");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:new_mask_sound");
+  event.create("satsu_iron_man_addon:fly_sound");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:nanomask");
+  event.create("satsu_iron_man_addon:mark_1_fire");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:entity.armor.shotgun.sound");
+  event.create("satsu_iron_man_addon:taken_teleport");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:entity.armor.mark.5.put.sound");
+  event.create("satsu_iron_man_addon:taken_damage");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:entity.armor.repulsor");
+  event.create("satsu_iron_man_addon:mark_6_40_suit_up");
 });
 StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:nano_weapons");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:entity.armor.holo_shield");
+  event.create("satsu_iron_man_addon:nanomask");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:new_beam");
+  event.create("satsu_iron_man_addon:mcunano");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:suitup");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:superior_enable");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:superior_weapon");
 });
 StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:red_laser");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:entity.armor.mark.46.mask.1");
+  event.create("satsu_iron_man_addon:call_parts_arm");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:entity.effect.dampened_sonic");
+  event.create("satsu_iron_man_addon:call_parts_another");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:call_parts_mask");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:call_parts_body");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:call_parts_legs");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:call_parts_head");
 });
 StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:entity.armor.walk.sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.run.sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.use.vibranuim.blades");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.repulsor");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.land");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.holo_shield");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.suit.up.taken");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.shotgun.sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.fly_sound.fast");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.mark.46.mask.1");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.mark.46.mask.2");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.mark.5.put.sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.emergency_glove.put.sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.effect.dampened_sonic");
 });
