e7d2578108348f8bf288934282fde0f8fcd34760
Satsu1312 <minion1312@hotmail.com>
2025-09-15 16:18:43 -0600

    renamed sound

--- addon/satsu_iron_man_addon/kubejs_scripts/sounds.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/sounds.js
@@ -32,7 +32,7 @@ StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:rivals_iron_man_steampower_blast_potencied");
 });
 StartupEvents.registry("sound_event", (event) => {
-  event.create("satsu_iron_man_addon:new_beam");
+  event.create("satsu_iron_man_addon:rivals_chest_beam");
 });
 StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:fly_sound");
