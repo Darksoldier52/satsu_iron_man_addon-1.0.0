e85ce4abdcb5df44153378e1900ae56ee7506c20
Satsu1312 <minion1312@hotmail.com>
2025-10-23 18:27:59 -0600

    Shield moved for position

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -237,3 +237,10 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
       event.registerProperty('satsu_iron_man_tech_knowledge_points_next_power_requier', 'integer', 1000);
     }
 });
+PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player" || "palladium:custom_projectile") {
+      // Arguments: Key of the property, type of the property, default/starting value
+      event.registerProperty('satsu_iron_man_nano_weapons_second', 'string', "none");
+    }
+});
