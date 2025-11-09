6f13aaf5540d72e51863956854e309f47a4de9fb
Satsu1312 <minion1312@hotmail.com>
2025-11-09 16:46:15 -0600

    marks pickaxe thing

--- data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -273,3 +273,10 @@ PalladiumEvents.registerProperties((event) => {
     event.registerProperty("satsu_iron_man_nano_counts_max", "integer", 10000);
   }
 });
+PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player" || "palladium:custom_projectile") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_ia.pickaxe_velocity", "string", "normal");
+  }
+});
